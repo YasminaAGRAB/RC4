cb8cc8ac0e28a9ebb1e27b8849b7a17ce20e9d11
YasminaAGRAB <95682722+YasminaAGRAB@users.noreply.github.com>
2021-12-07 12:29:41 +0100

    Add files via upload

---
+++ RC4/src/rc4/RC4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rc4;
+
+/**
+ *
+ * @author user_pc
+ */
+public class RC4 {
+    public byte[] key; 
+    public int[] sbox; 
+    public static final int SBOX_LENGTH = 256; 
+    public static final int KEY_MIN_LENGTH = 5; 
+    public int i = 0; 
+    public int j = 0; 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
